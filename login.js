aeee7c095da8a0661f8f9868b5cc8f76efc68cd6
tanukili <108317269+tanukili@users.noreply.github.com>
2023-01-13 17:46:39 +0800

    載入 vue

--- login.js
+++ login.js
@@ -1,5 +1,10 @@
-function hello() {
-    console.log('Hello!');
-};
+// vue 載入
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.45/vue.esm-browser.min.js';
 
-hello();
+createApp({
+    data() {
+        return {
+            counter: 5
+        }
+    },
+}).mount('#app')
